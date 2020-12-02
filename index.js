f462ec1a4064f0552c7015db258b697fb1a6052e
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-12-02 11:54:06 +0000

    file/dir structure

--- index.js
+++
@@ -1,5 +0,0 @@
-/* jshint -W014, -W033, esversion: 9 */
-/* eslint-disable new-cap */
-'use strict'
-const wemoPlatform = require('./lib/wemo-platform.js')
-module.exports = hb => hb.registerPlatform('homebridge-platform-wemo', 'BelkinWeMo', wemoPlatform)
